74a7dd984030a6311d35445341f0f0c9aad9f5a9
jeffreytwhitney <jeffrey.t.whitney@gmail.com>
2022-07-04 15:05:56 -0500

    Redoing how the UI will stay separate from the application code.

--- CompileSmArchive.bat
+++ CompileSmArchive.bat
@@ -1,4 +1,1 @@
-c:
-cd c:\temp\crap
-del *.* /q
-C:\Users\jeffr\AppData\Local\Programs\Python\Python310\Scripts\nuitka --onefile D:\OneDrive\Projects\SmArchiveQT\SmarchiveQT.py
+pyuic6 SmArchiveQT.ui -x -o SmArchive_UI.py
