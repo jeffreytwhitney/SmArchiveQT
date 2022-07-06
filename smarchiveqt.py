c94364e6c4d1ea2603cac29864f69ccc05ade5b7
jeffreytwhitney <jeffrey.t.whitney@gmail.com>
2022-07-06 05:34:32 -0500

    Pep 8 refactoring

--- smarchiveqt.py
+++ smarchiveqt.py
@@ -1,4 +1,4 @@
-from PyQt6 import QtCore, QtGui, QtWidgets
+from PyQt6 import QtCore, QtWidgets
 from PyQt6.QtWidgets import QMessageBox, QFileDialog
 from PyQt6.QtCore import Qt
 from operator import attrgetter
@@ -8,42 +8,44 @@ import sys
 from SmArchive_UI import Ui_SmArchiveMainWindow
 
 
-def showErrorMessage(message, title):
-    msgBox = QMessageBox()
-    msgBox.setIcon(QMessageBox.Icon.Critical)
-    msgBox.setText(message)
-    msgBox.setWindowTitle(title)
-    msgBox.setStandardButtons(QMessageBox.StandardButton.Ok)
-    msgBox.exec()
+def show_error_message(message, title):
+    msg_box = QMessageBox()
+    msg_box.setIcon(QMessageBox.Icon.Critical)
+    msg_box.setText(message)
+    msg_box.setWindowTitle(title)
+    msg_box.setStandardButtons(QMessageBox.StandardButton.Ok)
+    msg_box.exec()
 
 
-def showDuplicateFileDialog(source_file_path, archive_file_path):
-    sourceFile = Path(source_file_path)
-    archiveFile = Path(archive_file_path)
+def show_duplicate_file_dialog(source_file_path, archive_file_path):
+    source_file = Path(source_file_path)
+    archive_file = Path(archive_file_path)
 
-    size = sourceFile.stat().st_size
-    sourceFileSize = "{:,} bytes".format(sourceFile.stat().st_size)
-    sourceFileLastModified = datetime.fromtimestamp(sourceFile.stat().st_mtime)
+    source_file_size = "{:,} bytes".format(source_file.stat().st_size)
+    source_file_last_modified = datetime.fromtimestamp(source_file.stat().st_mtime)
 
-    archiveFileSize = "{:,} bytes".format(archiveFile.stat().st_size)
-    archiveFileLastModified = datetime.fromtimestamp(archiveFile.stat().st_mtime)
+    archive_file_size = "{:,} bytes".format(archive_file.stat().st_size)
+    archive_file_last_modified = datetime.fromtimestamp(archive_file.stat().st_mtime)
 
-    detailedText = f"SourceFile {sourceFile.name}: \n\tFileSize: {sourceFileSize}\n\tLast Modified: {sourceFileLastModified}\nArchiveFile {archiveFile.name}: \n\tFileSize: {archiveFileSize}\n\tLast Modified: {archiveFileLastModified}"
+    detailed_text = f"SourceFile {source_file.name}: \n\tFileSize: {source_file_size}\n\t" \
+                    f"Last Modified: {source_file_last_modified}\n" \
+                    f"ArchiveFile {archive_file.name}: \n\tFileSize: {archive_file_size}\n\t" \
+                    f"Last Modified: {archive_file_last_modified}"
 
-    msgBox = QMessageBox()
-    msgBox.setIcon(QMessageBox.Icon.Information)
-    msgBox.setText(f"File {archiveFile.name} already exists in archive directory. Replace?")
-    msgBox.setWindowTitle("Duplicate Archive File")
-    msgBox.setStandardButtons(QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
-    msgBox.setDetailedText(detailedText)
-    msgBox.exec()
-    return msgBox.clickedButton().text() == "&Yes"
+    msg_box = QMessageBox()
+    msg_box.setIcon(QMessageBox.Icon.Information)
+    msg_box.setText(f"File {archive_file.name} already exists in archive directory. Replace?")
+    msg_box.setWindowTitle("Duplicate Archive File")
+    msg_box.setStandardButtons(QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
+    msg_box.setDetailedText(detailed_text)
+    msg_box.exec()
+    return msg_box.clickedButton().text() == "&Yes"
 
 
-def getFilePathViaDialog():
+def get_file_path_via_dialog():
     dialog = QFileDialog()
-    returnPath = dialog.getExistingDirectory()
-    return str(Path(returnPath).absolute()) + "\\"
+    return_path = dialog.getExistingDirectory()
+    return str(Path(return_path).absolute()) + "\\"
 
 
 class SmArchiveMainWindow(QtWidgets.QMainWindow, Ui_SmArchiveMainWindow):
@@ -51,55 +53,56 @@ class SmArchiveMainWindow(QtWidgets.QMainWindow, Ui_SmArchiveMainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.btnSourceDirectory.clicked.connect(self.btnSourceDirectory_Clicked)
-        self.btnArchiveDirectory.clicked.connect(self.btnArchiveDirectory_Clicked)
-        self.chkOverwriteMode.stateChanged.connect(self.chkOverwriteMode_Checked)
-        self.chkSkipMode.stateChanged.connect(self.chkSkipModeChecked)
-        self.btnExecute.clicked.connect(self.btnExecute_Clicked)
+        self.btnSourceDirectory.clicked.connect(self.btn_source_directory_clicked)
+        self.btnArchiveDirectory.clicked.connect(self.btn_archive_directory_clicked)
+        self.chkOverwriteMode.stateChanged.connect(self.chk_overwrite_mode_checked)
+        self.chkSkipMode.stateChanged.connect(self.chk_skip_mode_checked)
+        self.btnExecute.clicked.connect(self.btn_execute_clicked)
 
-    def btnArchiveDirectory_Clicked(self):
-        self.txtArchiveDirectory.setText(getFilePathViaDialog())
+    def btn_archive_directory_clicked(self):
+        self.txtArchiveDirectory.setText(get_file_path_via_dialog())
 
-    def btnSourceDirectory_Clicked(self):
-        self.txtSourceDirectory.setText(getFilePathViaDialog())
+    def btn_source_directory_clicked(self):
+        self.txtSourceDirectory.setText(get_file_path_via_dialog())
 
-    def chkOverwriteMode_Checked(self, state):
+    def chk_overwrite_mode_checked(self, state):
         if QtCore.Qt.CheckState(state) == Qt.CheckState.Checked:
             self.chkSkipMode.setChecked(False)
 
-    def chkSkipModeChecked(self, state):
+    def chk_skip_mode_checked(self, state):
         if QtCore.Qt.CheckState(state) == Qt.CheckState.Checked:
             self.chkOverwriteMode.setChecked(False)
 
-    def btnExecute_Clicked(self):
+    def btn_execute_clicked(self):
         source_directory = self.txtSourceDirectory.text()
         archive_directory = self.txtArchiveDirectory.text()
         self.lstOutput.clear()
         if not Path(source_directory).exists():
-            showErrorMessage("Source Directory does not exist.", "Error")
+            show_error_message("Source Directory does not exist.", "Error")
             return
 
         if not Path(archive_directory).exists():
-            showErrorMessage("Archive Directory does not exist.", "Error")
+            show_error_message("Archive Directory does not exist.", "Error")
             return
 
         if str(source_directory).strip().upper() == str(archive_directory).strip().upper():
-            showErrorMessage("Source Directory and Archive Directory cannot be the same directory.", "Error")
+            show_error_message("Source Directory and Archive Directory cannot be the same directory.", "Error")
             return
 
         if self.txtFileExtension.text()[0] == "*":
             file_extension = self.txtFileExtension.text()[1:]
         else:
             file_extension = self.txtFileExtension.text()
+
         overwrite_mode = self.chkOverwriteMode.isChecked()
         test_mode = self.chkTestMode.isChecked()
         skip_mode = self.chkSkipMode.isChecked()
         file_suffix_length = int(self.lblFileNameSuffixLengthValue.text())
-        self.archiveFiles(source_directory, archive_directory, file_suffix_length, file_extension, overwrite_mode, skip_mode, test_mode)
+        self.archive_files(source_directory, archive_directory, file_suffix_length,
+                           file_extension, overwrite_mode, skip_mode, test_mode)
 
-    def archiveFiles(self, source_directory, archive_directory, file_suffix_length, file_extension, overwrite_mode,
-                     skip_mode,
-                     test_mode):
+    def archive_files(self, source_directory, archive_directory, file_suffix_length,
+                      file_extension, overwrite_mode, skip_mode, test_mode):
         allSourceFilesList = []
         uniqueSourceFilesList = []
         numberOfArchivedFiles = 0
@@ -119,71 +122,72 @@ class SmArchiveMainWindow(QtWidgets.QMainWindow, Ui_SmArchiveMainWindow):
             maxFilteredFile = max(filteredFileList, key=attrgetter('ArchiveFileSuffix'))
             for z in filteredFileList:
                 if z.ArchiveFileSuffix != maxFilteredFile.ArchiveFileSuffix:
-                    newFilePath = archive_directory + z.FileName + z.file_extension
-                    if self.archiveFile(z.FilePath, newFilePath, overwrite_mode, skip_mode, test_mode):
+                    newFilePath = archive_directory + z.FileName + z.FileExtension
+                    if self.archive_file(z.FilePath, newFilePath, overwrite_mode, skip_mode, test_mode):
                         numberOfArchivedFiles += 1
 
-        self.updateArchiveOutput(f"Total number of files moved: {numberOfArchivedFiles}")
+        self.update_archive_output(f"Total number of files moved: {numberOfArchivedFiles}")
 
-    def archiveFile(self, source_file_path, archive_file_path, overwrite_mode, skip_mode, test_mode):
+    def archive_file(self, source_file_path, archive_file_path, overwrite_mode, skip_mode, test_mode):
         if Path(archive_file_path).exists():
             if skip_mode is True:
                 return False
             if overwrite_mode:
                 if not test_mode:
                     Path(source_file_path).replace(archive_file_path)
-                    self.updateArchiveOutput(f"Archived file '{archive_file_path}'")
+                    self.update_archive_output(f"Archived file '{archive_file_path}'")
                     return True
                 else:
                     # test_mode = True
-                    self.updateArchiveOutput(f"Would have archived file '{archive_file_path}'")
+                    self.update_archive_output(f"Would have archived file '{archive_file_path}'")
                     return False
             else:
                 # overwrite_mode =  False
                 if not test_mode:
-                    if showDuplicateFileDialog(source_file_path, archive_file_path):
+                    if show_duplicate_file_dialog(source_file_path, archive_file_path):
                         Path(source_file_path).replace(archive_file_path)
-                        self.updateArchiveOutput(f"Archived file '{archive_file_path}'")
+                        self.update_archive_output(f"Archived file '{archive_file_path}'")
                         return True
                     else:
                         # User did not enter 'Y'
-                        self.updateArchiveOutput(f"Skipped file '{archive_file_path}'")
+                        self.update_archive_output(f"Skipped file '{archive_file_path}'")
                         return False
                 else:
                     # test_mode = True
-                    if showDuplicateFileDialog(source_file_path, archive_file_path):
-                        self.updateArchiveOutput(f"Would have archived file '{archive_file_path}'")
+                    if show_duplicate_file_dialog(source_file_path, archive_file_path):
+                        self.update_archive_output(f"Would have archived file '{archive_file_path}'")
                         return False
                     else:
                         # User did not enter 'Y'
-                        self.updateArchiveOutput(f"Skipped file '{archive_file_path}'")
+                        self.update_archive_output(f"Skipped file '{archive_file_path}'")
                         return False
 
         else:
             # Archive File does not already exist.
             if not test_mode:
                 Path(source_file_path).replace(archive_file_path)
-                self.updateArchiveOutput(f"Archived file '{archive_file_path}'")
+                self.update_archive_output(f"Archived file '{archive_file_path}'")
                 return True
             else:
                 # test_mode = False
-                self.updateArchiveOutput(f"Would have archived file '{archive_file_path}'")
+                self.update_archive_output(f"Would have archived file '{archive_file_path}'")
                 return False
 
-    def updateArchiveOutput(self, message):
+    def update_archive_output(self, message):
         self.lstOutput.addItem(message)
 
 
 class ArchiveFileInfo:
+
     def __init__(self, file_path: str, archive_file_suffix_length: int):
-        self.Archivefile_suffix_length = archive_file_suffix_length
+        self.FileSuffixLength = archive_file_suffix_length
         self.FilePath = file_path
-        self.file_extension = Path(file_path).suffix
+        self.FileExtension = Path(file_path).suffix
         self.FileName = Path(file_path).stem
-        uniqueFileNameLength = len(Path(file_path).stem) - self.Archivefile_suffix_length
+        uniqueFileNameLength = len(Path(file_path).stem) - self.FileSuffixLength
         self.UniqueFileName = self.FileName[:uniqueFileNameLength]
-        self.ArchiveFileSuffix = self.FileName[-self.Archivefile_suffix_length:]
-        self.Hash = self.UniqueFileName + self.file_extension
+        self.ArchiveFileSuffix = self.FileName[-self.FileSuffixLength:]
+        self.Hash = self.UniqueFileName + self.FileExtension
 
     def __eq__(self, other):
         return other.Hash == self.Hash
